7364895c634841464da7ad341e74aa6f8a6e1bc3
Micah Banschick <micha.ban@gmail.com>
2020-02-05 22:11:41 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,55 @@
+require 'pry'
+
+class Artist 
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+    self.save 
+  end 
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def add_song(song)
+    song.artist_name = @name
+    @songs << song   
+  end 
+  
+  def songs 
+    arr = []
+    Song.all.each do |song|
+      arr << song if song.artist.name == @name 
+    end
+    arr
+  end 
+  
+  def self.find_or_create_by_name(name)
+    self.find(name) ?  self.find(name) : self.create(name)
+  end
+
+  def self.find(name)
+    @@all.find {|artist| artist.name == name }
+  end
+  
+  def self.create(name)
+    art = self.new(name)
+    art.save 
+    art
+  end
+  
+  def print_songs
+    arr []
+    Song.all.each{|song| arr << song if song.artist.name == @name}
+  end 
+  #binding.pry
+end 
