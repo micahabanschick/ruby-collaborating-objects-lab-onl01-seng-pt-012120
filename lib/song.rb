1388cad8f4e3e9bff419486df4907e4a1b304efb
Micah Banschick <micha.ban@gmail.com>
2020-02-05 22:18:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ class Song
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1])
     song.artist_name = filename.split(" - ")[0]
-    #song
+    song
   end 
   
   def artist_name=(name)
